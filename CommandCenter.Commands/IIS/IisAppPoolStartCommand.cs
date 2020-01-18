af9e37cb9be5724d29e7e0d122bd9138be468e56
JABE <jabestrada@yahoo.com>
2020-01-18 12:53:02 +0800

    Added IisAppPoolStartCommand and IisAppPoolStopCommand

---
+++ CommandCenter.Commands/IIS/IisAppPoolStartCommand.cs
@@ -0,0 +1,33 @@
+﻿using CommandCenter.Commands.CmdLine;
+using CommandCenter.Infrastructure;
+
+namespace CommandCenter.Commands.IIS {
+    public class IisAppPoolStartCommand : BaseCmdLineCommand {
+        public string AppPoolName { get; protected set; }
+        public IisAppPoolStartCommand(string cmdPath, string appPoolName) {
+            Executable = cmdPath;
+            AppPoolName = appPoolName;
+        }
+        protected override void SetArguments() {
+            CommandLineArguments.Add("start");
+            CommandLineArguments.Add("apppool");
+            CommandLineArguments.Add($"/apppool.name:\"{AppPoolName}\"");
+        }
+        protected override void OnCommandWillRun() {
+            SendReport($"Starting IIS app pool \"{AppPoolName}\"...", ReportType.Progress);
+        }
+        protected override void OnCommandDidRun() {
+            if (DidCommandSucceed) {
+                SendReport($"Successfully STARTED IIS app pool \"{AppPoolName}\"", ReportType.DoneTaskWithSuccess);
+            }
+            else {
+                SendReport($"FAILED to start app pool \"{AppPoolName}\" with exit code {ExitCode}", ReportType.DoneTaskWithFailure);
+            }
+        }
+        protected override void OnOutputStreamDataIn(string data) {
+            if (!string.IsNullOrWhiteSpace(data)) {
+                SendReport($"IisAppPoolStartCommand => {data}", ReportType.Progress);
+            }
+        }
+    }
+}
---
+++ CommandCenter.Commands/IIS/IisAppPoolStopCommand.cs
@@ -0,0 +1,44 @@
+﻿using CommandCenter.Commands.CmdLine;
+using CommandCenter.Infrastructure;
+
+namespace CommandCenter.Commands.IIS {
+    public class IisAppPoolStopCommand : BaseCmdLineCommand {
+        public string AppPoolName { get; protected set; }
+        public bool AlreadyStoppedPrior { get; protected set; }
+        public IisAppPoolStopCommand(string cmdPath, string appPoolName) {
+            Executable = cmdPath;
+            AppPoolName = appPoolName;
+        }
+        protected override void SetArguments() {
+            CommandLineArguments.Add("stop");
+            CommandLineArguments.Add("apppool");
+            CommandLineArguments.Add($"/apppool.name:\"{AppPoolName}\"");
+        }
+        protected override void OnCommandWillRun() {
+            AlreadyStoppedPrior = false;
+            SendReport($"Stopping IIS app pool \"{AppPoolName}\"...", ReportType.Progress);
+        }
+        protected override void OnCommandDidRun() {
+            if (DidCommandSucceed) {
+                SendReport($"Successfully STOPPED IIS app pool \"{AppPoolName}\"", ReportType.DoneTaskWithSuccess);
+            }
+            else {
+                if (AlreadyStoppedPrior) {
+                    DidCommandSucceed = true;
+                    SendReport($"IIS app pool \"{AppPoolName}\" already STOPPED before command executed", ReportType.DoneTaskWithSuccess);
+                }
+                else {
+                    SendReport($"FAILED to stop app pool \"{AppPoolName}\" with exit code {ExitCode}", ReportType.DoneTaskWithFailure);
+                }
+            }
+        }
+        protected override void OnOutputStreamDataIn(string data) {
+            if (!string.IsNullOrWhiteSpace(data)) {
+                if (data.Contains("already stopped")) {
+                    AlreadyStoppedPrior = true;
+                }
+                SendReport($"IisAppPoolStopCommand => {data}", ReportType.Progress);
+            }
+        }
+    }
+}
