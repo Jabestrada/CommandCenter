5ea27689e8784cdee739da3c408a1127167acd03
JABE <jabestrada@yahoo.com>
2020-02-22 21:39:46 +0800

    Added WinForms.ConditionalPauseCommand

---
+++ CommandCenter.Commands/WinForms/ConditionalPauseCommand.cs
@@ -0,0 +1,21 @@
+﻿using CommandCenter.Infrastructure;
+using CommandCenter.Infrastructure.Orchestration;
+using System;
+using System.Windows.Forms;
+
+namespace CommandCenter.Commands.WinForms {
+    public class ConditionalPauseCommand : BaseCommand {
+        public string[] PromptTexts { get; protected set; }
+        public override bool IsUndoable => false;
+        public ConditionalPauseCommand(params string[] linesOfText) {
+            PromptTexts = linesOfText;
+        }
+        public override void Do() {
+            var userReply = MessageBox.Show(string.Join(Environment.NewLine, PromptTexts), "User Input Needed", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            DidCommandSucceed = userReply == DialogResult.Yes;
+            var cmdResult = DidCommandSucceed ? "succeeded" : "failed";
+            SendReport(this, $"ConditionalPause => Command {cmdResult} because user responded with {userReply}", 
+                       DidCommandSucceed ? ReportType.DoneTaskWithSuccess : ReportType.DoneTaskWithFailure);
+        }
+    }
+}
