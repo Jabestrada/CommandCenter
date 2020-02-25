8959c9eb2eb4f00396f9636a931f875aeede3f4c
JABE <jabestrada@yahoo.com>
2020-02-25 22:42:00 +0800

    Added Undo for IisAppPoolStartCommand

--- CommandCenter.Commands/IIS/IisAppPoolStartCommand.cs
+++ CommandCenter.Commands/IIS/IisAppPoolStartCommand.cs
@@ -3,6 +3,7 @@ using CommandCenter.Infrastructure.Orchestration;
 
 namespace CommandCenter.Commands.IIS {
     public class IisAppPoolStartCommand : BaseCmdLineCommand {
+        public override bool IsUndoable => true;
         public string AppPoolName { get; protected set; }
         public IisAppPoolStartCommand(string cmdPath, string appPoolName) {
             Executable = cmdPath;
@@ -27,6 +28,27 @@ namespace CommandCenter.Commands.IIS {
         protected override void OnOutputStreamDataIn(string data) {
             if (!string.IsNullOrWhiteSpace(data)) {
                 SendReport($"IisAppPoolStartCommand => {data}", ReportType.Progress);
+            }
+        }
+        public override void Undo() {
+            if (!DidCommandSucceed) {
+                SendReport($"IisAppPoolStartCommand => Not stopping app pool {AppPoolName} on Undo because Do failed", ReportType.UndoneTaskWithSuccess);
+                return;
+            }
+
+
+            SendReport($"IisAppPoolStartCommand => Attempting to stop app pool {AppPoolName} on Undo...", ReportType.Progress);
+            var startArgs = CommandLineArguments;
+            startArgs[0] = "stop";
+            using (CommandLineProcessRunner cmd = new CommandLineProcessRunner(Executable, true, string.Join(" ", startArgs))) {
+                var exitCode = cmd.Run(outputStreamReceiver, errorStreamReceiver);
+                if (exitCode == SuccessExitCode) {
+                    SendReport($"IisAppPoolStartCommand => Successfully stopped app pool {AppPoolName} on Undo", ReportType.UndoneTaskWithSuccess);
+                }
+                else {
+                    SendReport($"IisAppPoolStartCommand => Failed to stop app pool {AppPoolName} on Undo", ReportType.UndoneTaskWithFailure);
+
+                }
             }
         }
     }
